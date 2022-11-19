4d89c234784013b852ce2c919dc2d8a891cc44c0
fiona <fiona.kitchen@yahoo.co.uk>
2022-11-19 15:04:01 +0000

    i've made a webpage which displays from a sql lite data base and links to it on a seperate page - i'm on 7.3.1 of the guide which is a form builder so will pick up there

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,25 @@
 class ArticlesController < ApplicationController
   # this means it inherits from appliction controller
   def index
+    #@articles is an instance
+  @articles = Article.all
+  end
+
+  def show 
+    @article = Article.find(params[:id])
+  end
+
+  def new 
+    @article= Article.new
+  end 
+
+  def create
+    @article= Article.new(title: "...", body: "...")
+
+    if @article.save 
+      redirect_to @article
+    else 
+      render :new, status::unprocessable_entity
+    end 
   end
 end
